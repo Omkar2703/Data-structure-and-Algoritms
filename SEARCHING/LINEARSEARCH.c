3bef5c334bde561e0be606d60624b54d4f0bc57b
Omkar2703 <omkar.ptl556@gmail.com>
2022-08-22 20:18:18 +0530

    DSA

---
+++ SEARCHING/LINEARSEARCH.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+int linearsearch( int a[], int n, int x)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(x==a[i])
+        {
+            printf("\nElement found at %d", i);
+        }
+        else
+        {
+            printf("\nElement not found at %d", i);
+        }
+    }
+}
+int main()
+{
+    int *a, n, x, i;
+    printf("\nEnter the number elements:");
+    scanf("%d", &n);
+    a=(int*)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        printf("\nEnter the elements:");
+        scanf("%d", &a[i]);
+    }
+    printf("\nEnter the element to be search:");
+    scanf("%d", &x);
+    printf("\nElements in array are....\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t", a[i]);
+    }
+    linearsearch(a , n, x);
+    return 0;
+}
