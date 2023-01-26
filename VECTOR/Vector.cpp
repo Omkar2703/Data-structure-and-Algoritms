3d48db748fbc13368db847141539063b1ec6a24d
Omkar_Patil <omkar.ptl556@gmail.com>
2023-01-26 13:55:32 +0530

    commit

--- VECTOR/Vector.cpp
+++ VECTOR/Vector.cpp
@@ -18,6 +18,9 @@ int main(){
 		cin>>x;
 		p.push_back(x);
 	}
+	cout<<"Size of Vector is "<<p.size()<<endl;
+	cout<<"Capacity of Vector is "<<p.capacity()<<endl;
+	cout<<"Max of Vector is "<<p.max_size();
 	cout<<"\nEntered elements are...\n";
 	display(p);
 }
